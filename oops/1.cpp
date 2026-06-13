abe00c00769059c6b26791737f4c1232fe7335a3
voidaditya <dypatil.aditya00@gmail.com>
2026-06-13 14:54:33 +0530

    Add files via upload

--- oops/1.cpp
+++ oops/1.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 class MyClass {
 public:
-  void myFunction() { cout << "Some content in parent class."; }
+  void myFunction() { cout << "Some content in parent class." << endl; }
 };
 
 class MyOtherClass {
 public:
-  void myOtherFunction() { cout << "Some content in another class."; }
+  void myOtherFunction() { cout << "Some content in another class." << endl; }
 };
 
 class MyChildClass : public MyClass, public MyOtherClass {};
@@ -21,26 +21,48 @@ private:
 public:
   int setDistance(int *d) {
     distance = *d;
-
     return distance;
   }
 
   int getDistance() { return distance; }
 };
 
+class Animal {
+public:
+  string sound(string sound) { return sound; }
+};
+
+class Dog : public Animal {
+public:
+  string sound(string sound) { return sound; }
+};
+
 class Km : public Location {};
+
 int main() {
+  Animal ani;
+  Dog d;
   Location location;
   Km km;
+
   int value = 200;
   location.setDistance(&value);
-  cout << location.getDistance();
+  cout << "Distance in Location: " << location.getDistance() << endl;
+
   int valuek = 500;
   km.setDistance(&valuek);
-  cout << km.getDistance();
+  cout << "Distance in Km: " << km.getDistance() << endl;
+
   MyChildClass myObj;
+
+  cout << "Calling MyClass function:" << endl;
   myObj.myFunction();
+
+  cout << "Calling MyOtherClass function:" << endl;
   myObj.myOtherFunction();
+
+  cout << "Animal sound: " << ani.sound("different sounds") << endl;
+  cout << "Dog sound: " << d.sound("bark") << endl;
 
   return 0;
 }
