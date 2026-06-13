0d1a034423a2654bb365c4f87b53a6d9620e96e9
voidaditya <dypatil.aditya00@gmail.com>
2026-06-13 14:01:02 +0530

    Add files via upload

--- oops/1.cpp
+++ oops/1.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class MyClass {
+public:
+  void myFunction() { cout << "Some content in parent class."; }
+};
+
+class MyOtherClass {
+public:
+  void myOtherFunction() { cout << "Some content in another class."; }
+};
+
+class MyChildClass : public MyClass, public MyOtherClass {};
+
 class Location {
 private:
   int distance;
@@ -9,17 +21,26 @@ private:
 public:
   int setDistance(int *d) {
     distance = *d;
+
     return distance;
   }
 
   int getDistance() { return distance; }
 };
 
+class Km : public Location {};
 int main() {
   Location location;
+  Km km;
   int value = 200;
   location.setDistance(&value);
   cout << location.getDistance();
+  int valuek = 500;
+  km.setDistance(&valuek);
+  cout << km.getDistance();
+  MyChildClass myObj;
+  myObj.myFunction();
+  myObj.myOtherFunction();
 
   return 0;
 }
