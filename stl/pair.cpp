eaba56f5e01f29b7d02c2e89f7dc138744e3add8
voidaditya <dypatil.aditya00@gmail.com>
2026-06-17 01:49:30 +0530

    Add files via upload

---
+++ stl/pair.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   pair<int,string> x;
+   x={22,"aditya"};
+   cout<<x.first<<" "<<x.second;
+   
+
+   pair<int,string> x1[4] = {
+    {1,"adi"},
+    {2,"adit"},
+    {3,"aditya"},
+    {4,"Aditya"}
+};
+   cout<<"\n";
+
+   
+   	cout<<x1[0].first<<" "<<x1[0].second<<endl;
+   	cout<<x1[1].first<<" "<<x1[1].second<<endl;
+  
+
+	return 0;
+}
